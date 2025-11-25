e697b5e4b5a1f41473def735cca780f4b3e0fab7
Shalini <shalininancylammetier7@gmail.com>
2025-11-25 15:15:51 +0800

    style fixes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,3 @@
-// import { Component,signal } from '@angular/core';
-// import { RouterOutlet, RouterLink } from '@angular/router';
-// import { CommonModule } from '@angular/common';
-
-// @Component({
-//   selector: 'app-root',
-//   standalone: true,
-//   imports: [
-//     CommonModule,
-//     RouterOutlet,
-//     RouterLink   // ✅ REQUIRED for <a routerLink>
-//   ],
-//   templateUrl: './app.html',
-//   styleUrls: ['./app.css']
-// })
-// export class App {
-//     protected readonly title = signal('CarRegistrationAppFrontEnd');
-// }
-
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
